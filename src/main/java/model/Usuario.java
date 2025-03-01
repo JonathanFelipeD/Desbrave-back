3b3c206b0c3f08f4b1975644a4620f1994ec75a7
jonathan <felipe.jonathanjf@gmail.com>
2025-03-01 19:13:16 -0300

    primeiro commit

---
+++ src/main/java/model/Usuario.java
@@ -0,0 +1,19 @@
+package model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+
+    @Column(nullable = false, length = 50)
+    private String nome;
+}   
