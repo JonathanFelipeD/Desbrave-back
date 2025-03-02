05564d6ba893726ee91f1a9199fe63e9894f1048
jonathan <felipe.jonathanjf@gmail.com>
2025-03-02 19:40:14 -0300

    teste do git

--- src/main/java/model/Usuario.java
+++ src/main/java/model/Usuario.java
@@ -2,6 +2,9 @@ package model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,6 +16,10 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "usuarios")
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
 
     @Column(nullable = false, length = 50)
     private String nome;
