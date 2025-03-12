8158f3667dc9d696ffcf82a7e06ea1911ff3ad7e
jonathan <felipe.jonathanjf@gmail.com>
2025-03-11 22:10:33 -0300

    criação model usuarioCupom

---
+++ src/main/java/com/Desbrave/Desbrave/model/UsuarioCupom.java
@@ -0,0 +1,52 @@
+package com.Desbrave.Desbrave.model;
+
+
+import com.Desbrave.Desbrave.constants.Usado;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table
+public class UsuarioCupom {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idUsuarioCupom;
+
+    @Column(nullable =  false)
+    private LocalDate dataResgate;
+
+    @Column(nullable = false)
+    private Usado Usado;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id")
+    private Usuario usuario;
+
+    // @ManyToOne
+    // @JoinColumn(name = "cursos_id")
+    // private Cursos cursos;
+
+
+    @PrePersist
+    protected void onCreate(){
+        this.dataResgate = LocalDate.now();
+    }
+
+}
