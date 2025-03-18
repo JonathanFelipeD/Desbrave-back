9e7f49f1ce66797932756c3ca11435f8694202a6
jonathan <felipe.jonathanjf@gmail.com>
2025-03-17 21:09:52 -0300

    implementação do sistema de autenticação com JWT, incluindo classes de configuração, serviço e controlador

--- src/main/java/com/Desbrave/Desbrave/repository/UsuarioRepository.java
+++ src/main/java/com/Desbrave/Desbrave/repository/UsuarioRepository.java
@@ -1,9 +1,13 @@
 package com.Desbrave.Desbrave.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.Desbrave.Desbrave.model.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
+
+    Optional<Usuario> findByEmail(String email);
 
     
     
